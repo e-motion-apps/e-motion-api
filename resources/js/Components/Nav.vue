ee788a6e4b4b41ca56bf5f568008bd6232d2f69d
Bartosz Brzózka <brzozka108@gmail.com>
2023-06-12 15:15:34 +0200

    Adding logo component and main page template adjustment

--- resources/js/Components/Nav.vue
+++ resources/js/Components/Nav.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import { Dialog, DialogPanel } from '@headlessui/vue'
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+
   
 const navigation = [
   { name: 'Language', href: '#' },
@@ -13,18 +14,20 @@ const mobileMenuOpen = ref(false)
   
 <template>
   <header class="bg-white">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-      <a href="#" class="-m-1.5 p-1.5">
-        <span class="sr-only">Your Company</span>
-        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+    <nav class="mx-auto flex max-w-full items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <a href="#" class="-m-1.5 flex items-center p-1.5">
+        <img class="h-12 w-auto" src="@/assets/scooter.png" alt="">
+        <span class=" ml-1 text-3xl font-semibold ">Escooters</span>
       </a>
+      
       <div class="flex lg:hidden">
         <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
           <span class="sr-only">Open main menu</span>
           <Bars3Icon class="h-6 w-6" aria-hidden="true" />
         </button>
       </div>
-      <div class="hidden lg:flex lg:gap-x-12">
+      
+      <div class="hidden items-center lg:flex lg:gap-x-12">
         <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</a>
         <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
       </div>
@@ -33,9 +36,9 @@ const mobileMenuOpen = ref(false)
       <div class="fixed inset-0 z-10" />
       <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
         <div class="flex items-center justify-between">
-          <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+          <a href="#" class="-m-1.5 flex items-center p-1.5">
+            <img class="h-8 w-auto" src="@/assets/scooter.png" alt="">
+            <span class="ml-1 ">Escooters</span>
           </a>
           <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
             <span class="sr-only">Close menu</span>
--- resources/js/Components/Test.vue
+++ resources/js/Components/Test.vue
@@ -3,27 +3,28 @@ import Map from './Map.vue'
 </script>
 
 <template>
-  <div class="mx-auto grid min-h-screen grid-cols-1 grid-rows-[1fr,auto,1fr] bg-white lg:grid-cols-[max(50%,36rem),1fr]">
-    <main class="mx-auto w-full max-w-7xl px-6 py-24 sm:py-32 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8">
+  <div class="mx-auto grid min-h-full grid-cols-2 grid-rows-[1fr,auto,1fr] bg-white lg:grid-cols-[max(50%,36rem),1fr]">
+    <div class="mx-auto w-full max-w-full px-6 py-24 sm:py-32 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8">
       <div class="max-w-lg">
-        <p class="text-base font-semibold leading-8 text-indigo-600">
-          404
-        </p>
+        <div class="p-4">
+          <img src="@/assets/undraw_scooter.svg" alt="Man riding a e-scooter" class="mx-auto">
+        </div>
         <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Page not found
+          Looking for an e-scooter?
         </h1>
         <p class="mt-6 text-base leading-7 text-gray-600">
-          Sorry, we couldn’t find the page you’re looking for.
+          There are plenty of e-scooter providers, so sometimes you can be confused which app should you use during your trips. I know this problem. This little map can help you with planning your vacations or business trips.
         </p>
-        <div class="mt-10">
-          <a href="#" class="text-sm font-semibold leading-7 text-indigo-600"><span aria-hidden="true">&larr;</span> Back to home</a>
-        </div>
       </div>
-    </main>
-    
-    <div class="hidden lg:relative lg:col-start-2 lg:row-start-1 lg:row-end-4 lg:block">
-      <Map class="map" />
     </div>
+    
+    
+    <Map class="map hidden lg:relative lg:col-start-2 lg:row-start-1 lg:row-end-4 lg:block" />
   </div>
 </template>
+
+
+
+
+
   
