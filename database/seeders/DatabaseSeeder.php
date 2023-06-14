83407251511752d83bcf1965534ff2df361b4eb3
vojcc <kuba.wojcik000@gmail.com>
2023-06-14 15:42:36 +0200

    #4 - remove unrelated factories calls

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use App\Models\City;
-use App\Models\CityAlternativeName;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
