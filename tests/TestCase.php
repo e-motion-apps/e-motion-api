b3ec8bf79bbaf2ab49a566dbe2ffaed219d35cc8
vojcc <kuba.wojcik000@gmail.com>
2023-06-14 12:22:01 +0200

    #4 - TestCase.php fix

---
+++ tests/TestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
+}
