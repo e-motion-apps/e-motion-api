dd04e33caf9da70a0816fe326c8d6d66962ea442
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2023-07-13 15:40:12 +0200

    #13 - Notes visible only for creator

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Note;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
@@ -14,25 +15,37 @@ class NoteController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $notes = Note::where("user_id", $user->id)->orderBy("created_at", "desc")->pluck("text");
+        $notes = Note::where("user_id", $user->id)->orderBy("created_at", "desc")->get();
 
         return Inertia::render("Notes/Index", [
             "notes" => $notes,
         ]);
-    }    
+    }
 
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         $user = Auth::user();
         $notes = $request->input("noteText", []);
-    
+
+        if (!is_array($notes)) {
+            $notes = [$notes];
+        }
+
         foreach ($notes as $note) {
             if ($note !== null) {
                 $user->notes()->create(["text" => $note]);
             }
         }
-    
+
         return redirect()->back();
     }
-    
+
+    public function destroy(Note $note): RedirectResponse
+    {
+        $note->delete();
+
+        return redirect()
+            ->back()
+            ->with("success", __("Note has been deleted"));
+    }
 }
--- resources/js/Pages/Notes/Index.vue
+++ resources/js/Pages/Notes/Index.vue
@@ -1,54 +1,58 @@
 <script setup>
-import { defineProps, ref } from 'vue';
-import { Inertia } from '@inertiajs/inertia';
-
+import { defineProps, ref } from 'vue'
+import { Inertia } from '@inertiajs/inertia'
+import { TrashIcon  } from '@heroicons/vue/24/solid'
 const props = defineProps({
   notes: Array,
-});
+})
 
-const noteText = ref('');
+const noteText = ref('')
 
 const saveNotes = () => {
-  const noteValues = props.notes.map((note) => note.text);
-  Inertia.post('/notes', { noteText: noteValues });
+  const noteValues = props.notes.map((note) => note.text)
+  Inertia.post('/notes', { noteText: noteValues })
 }
 
 const addNote = () => {
   if (noteText.value) {
-    props.notes.push({ text: noteText.value });
-    noteText.value = '';
-    saveNotes();
+    props.notes.push({ text: noteText.value })
+    noteText.value = ''
+    saveNotes()
   }
 }
 
-const deleteNote = (index) => {
-  props.notes.splice(index, 1);
-  saveNotes();
-};
 </script>
 
 
 <template>
-    <div>
-      <div class="note-container flex mb-4">
-        <div class="relative w-3/4">
-          <textarea v-model="noteText" class="note-input w-full h-24 px-2 py-1" placeholder="Write your note here"></textarea>
-          <button class="note-button rounded bg-green-500 px-3 py-3 font-semibold text-white text-sm absolute top-1/2 right-2 transform -translate-y-1/2" @click="addNote">
-            Add Note
-          </button>
-        </div>
+  <div>
+    <div class="note-container mb-4 flex">
+      <div class="relative w-3/4">
+        <textarea v-model="noteText" class="note-input h-24 w-full px-2 py-1" placeholder="Write your note here" />
+        <button class="note-button absolute right-2 top-1/2 -translate-y-1/2 rounded bg-green-500 p-3 text-sm font-semibold text-white" @click="addNote">
+          Add Note
+        </button>
       </div>
-  
-      <ul class="note-list">
-        <li v-for="(note, index) in props.notes" :key="index" class="note-item mb-4">
-          <div class="border border-gray-300 bg-gray-100 p-4 rounded-lg w-1/4 relative">
-            <p class="grow">{{ note }}</p>
-            <button class="note-delete rounded bg-red-500 px-3 py-1 font-semibold text-white mt-2 text-sm absolute top-2 right-2" @click="deleteNote(index)">
-              Delete
-            </button>
-          </div>
-        </li>
-      </ul>
     </div>
-  </template>
-  
+    <ul class="note-list">
+      <li v-for="(note, index) in props.notes" :key="index" class="note-item mb-4">
+        <div class="relative w-1/4 rounded-lg border border-gray-300 bg-gray-100 p-4">
+          <p class="grow">
+            {{ note.text }}
+          </p>
+          <div class="absolute right-2 top-2">
+            <InertiaLink
+              as="button"
+              method="delete"
+              :preserve-scroll="true"
+              :href="`/notes/${note.id}`"
+              :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-left text-sm font-medium']"
+            >
+              <TrashIcon class="mr-2 h-7 w-7 text-red-500" aria-hidden="true" />
+            </InertiaLink>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
