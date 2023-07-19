4fbecabd82dbbca2022aa3c36d3cea11c43302fe
vojcc <kuba.wojcik000@gmail.com>
2023-07-19 09:50:08 +0200

    #51 - add typing, remove unnecessary route and controller

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\CityProviderController;
 use App\Http\Controllers\CountryController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Response;
 
 Route::middleware("guest")->group(function (): void {
     Route::post("/login", [AuthController::class, "login"])->name("login");
@@ -24,7 +23,6 @@ Route::middleware("auth")->group(function (): void {
         Route::resource("/admin/dashboard/countries", CountryController::class);
         Route::resource("/admin/dashboard/cities", CityController::class);
         Route::resource("/city-alternative-name", CityAlternativeNameController::class);
-        Route::get("/admin", fn(): Response => inertia("Admin"))->name("admin");
         Route::patch("/update-city-providers/{city}", [CityProviderController::class, "update"]);
     });
 });
