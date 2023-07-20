87f38a7299804bb300abdd2a1ccbcf74aaf2e5cf
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2023-07-20 10:13:33 +0200

    simplified

--- resources/js/Shared/Layout/AdminNavigation.vue
+++ resources/js/Shared/Layout/AdminNavigation.vue
@@ -21,7 +21,9 @@ function toggleMobileMenu() {
 </script>
 
 <template>
-  <div class="fixed z-10 flex w-full border-blumilk-50 bg-blumilk-25 shadow md:h-full md:w-1/3 md:border-r lg:w-1/4 xl:w-1/6">
+  <div
+    class="fixed z-10 flex w-full border-blumilk-50 bg-blumilk-25 shadow md:h-full md:w-1/3 md:border-r lg:w-1/4 xl:w-1/6"
+  >
     <div class="flex w-full justify-between md:flex-col md:justify-normal">
       <Link href="/" class="mt-3 flex shrink-0 items-center pb-3 pl-6 pr-2">
         <img class="h-10" src="@/assets/scooter.png" alt="escooter logo">
@@ -29,19 +31,27 @@ function toggleMobileMenu() {
       </Link>
 
       <div class="mr-3.5 flex sm:hidden">
-        <button type="button" class="inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="toggleMobileMenu">
+        <button type="button" class="inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                @click="toggleMobileMenu"
+        >
           <span class="sr-only">Open main menu</span>
           <Bars3Icon class="h-6 w-6" aria-hidden="true" />
         </button>
       </div>
-      <Dialog v-if="isMobileMenuOpened" as="div" class="z-30 lg:hidden" :open="isMobileMenuOpened" @close="toggleMobileMenu">
+      <Dialog v-if="isMobileMenuOpened" as="div" class="z-30 lg:hidden" :open="isMobileMenuOpened"
+              @close="toggleMobileMenu"
+      >
         <div class="fixed inset-0 z-30" />
-        <DialogPanel class="fixed inset-y-0 right-0 z-30 w-full overflow-y-auto border-b-2 bg-white px-6 py-3 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel
+          class="fixed inset-y-0 right-0 z-30 w-full overflow-y-auto border-b-2 bg-white px-6 py-3 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+        >
           <div class="flex items-center justify-between sm:justify-end">
             <Link href="/">
               <img class="h-10 sm:hidden" src="@/assets/scooter.png" alt="escooter logo">
             </Link>
-            <button type="button" class="-m-2.5 rounded-md px-2.5 text-gray-700 sm:pt-4" @click="toggleMobileMenu">
+            <button type="button" class="-m-2.5 rounded-md px-2.5 text-gray-700 sm:pt-4"
+                    @click="toggleMobileMenu"
+            >
               <span class="sr-only">Close menu</span>
               <XMarkIcon class="h-6 w-6" aria-hidden="true" />
             </button>
@@ -49,7 +59,10 @@ function toggleMobileMenu() {
           <div class="mt-6 flow-root">
             <div class="-my-6 divide-y divide-gray-500/10">
               <div class="space-y-2 py-6">
-                <Link v-for="item in navigation" :key="item.name" :class="$page.url.startsWith(item.href)? 'bg-blumilk-50' : ''" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-800 hover:bg-blumilk-25" @click="clearFilters">
+                <Link v-for="item in navigation" :key="item.name"
+                      :class="{'bg-blumilk-50': $page.url.startsWith(item.href)}" :href="item.href"
+                      class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-800 hover:bg-blumilk-25"
+                >
                   {{ item.name }}
                 </Link>
               </div>
@@ -60,7 +73,7 @@ function toggleMobileMenu() {
 
       <ul class="hidden h-full items-center text-sm font-medium text-gray-800 sm:flex md:mt-12 md:flex-col md:items-stretch md:space-y-2">
         <Link v-for="item in navigation" :key="item.name" :href="item.href" class="flex h-full md:h-fit">
-          <div :class="$page.url.startsWith(item.href)? 'bg-blumilk-50' : ''"
+          <div :class="{'bg-blumilk-50': $page.url.startsWith(item.href)}"
                class="mx-auto flex w-11/12 items-center bg-blumilk-25 px-6 hover:bg-blumilk-50 md:rounded-lg md:px-2 md:py-3"
           >
             <component :is="item.icon" class="h-7 w-7" />
