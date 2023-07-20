43011f5747644eec6b7d977781f4da04315746d9
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2023-07-20 10:29:41 +0200

    Links changed to InertiaLinks

--- resources/js/Shared/Layout/AdminNavigation.vue
+++ resources/js/Shared/Layout/AdminNavigation.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { Dialog, DialogPanel } from '@headlessui/vue'
-import { Link } from '@inertiajs/vue3'
+import { InertiaLink } from '@inertiajs/vue3'
 import { ChartBarIcon, ClipboardIcon, FlagIcon, MapPinIcon, PlayCircleIcon } from '@heroicons/vue/24/solid'
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 import { ref } from 'vue'
@@ -25,10 +25,10 @@ function toggleMobileMenu() {
     class="fixed z-10 flex w-full border-blumilk-50 bg-blumilk-25 shadow md:h-full md:w-1/3 md:border-r lg:w-1/4 xl:w-1/6"
   >
     <div class="flex w-full justify-between md:flex-col md:justify-normal">
-      <Link href="/" class="mt-3 flex shrink-0 items-center pb-3 pl-6 pr-2">
+      <InertiaLink href="/" class="mt-3 flex shrink-0 items-center pb-3 pl-6 pr-2">
         <img class="h-10" src="@/assets/scooter.png" alt="escooter logo">
         <span class="ml-3 hidden text-2xl font-semibold text-gray-800 md:flex">e&#8209;scooters</span>
-      </Link>
+      </InertiaLink>
 
       <div class="mr-3.5 flex sm:hidden">
         <button type="button" class="inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -46,9 +46,9 @@ function toggleMobileMenu() {
           class="fixed inset-y-0 right-0 z-30 w-full overflow-y-auto border-b-2 bg-white px-6 py-3 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
         >
           <div class="flex items-center justify-between sm:justify-end">
-            <Link href="/">
+            <InertiaLink href="/">
               <img class="h-10 sm:hidden" src="@/assets/scooter.png" alt="escooter logo">
-            </Link>
+            </InertiaLink>
             <button type="button" class="-m-2.5 rounded-md px-2.5 text-gray-700 sm:pt-4"
                     @click="toggleMobileMenu"
             >
@@ -59,12 +59,12 @@ function toggleMobileMenu() {
           <div class="mt-6 flow-root">
             <div class="-my-6 divide-y divide-gray-500/10">
               <div class="space-y-2 py-6">
-                <Link v-for="item in navigation" :key="item.name"
+                <InertiaLink v-for="item in navigation" :key="item.name"
                       :class="{'bg-blumilk-50': $page.url.startsWith(item.href)}" :href="item.href"
                       class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-800 hover:bg-blumilk-25"
                 >
                   {{ item.name }}
-                </Link>
+                </InertiaLink>
               </div>
             </div>
           </div>
@@ -72,14 +72,14 @@ function toggleMobileMenu() {
       </Dialog>
 
       <ul class="hidden h-full items-center text-sm font-medium text-gray-800 sm:flex md:mt-12 md:flex-col md:items-stretch md:space-y-2">
-        <Link v-for="item in navigation" :key="item.name" :href="item.href" class="flex h-full md:h-fit">
+        <InertiaLink v-for="item in navigation" :key="item.name" :href="item.href" class="flex h-full md:h-fit">
           <div :class="{'bg-blumilk-50': $page.url.startsWith(item.href)}"
                class="mx-auto flex w-11/12 items-center bg-blumilk-25 px-6 hover:bg-blumilk-50 md:rounded-lg md:px-2 md:py-3"
           >
             <component :is="item.icon" class="h-7 w-7" />
             <span class="ml-3 hidden md:flex"> {{ item.name }} </span>
           </div>
-        </Link>
+        </InertiaLink>
       </ul>
     </div>
   </div>
