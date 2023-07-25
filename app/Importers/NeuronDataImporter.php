f8b53987412b95a3e6a70b2ad7d9e5be96d265d1
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2023-07-25 08:53:29 +0200

    change parsing data

--- app/Importers/NeuronDataImporter.php
+++ app/Importers/NeuronDataImporter.php
@@ -17,9 +17,12 @@ class NeuronDataImporter extends DataImporter
     private const PROVIDER_ID = 9;
 
     protected Crawler $sections;
+    protected string $iso;
+    protected $jsonData;
 
     public function extract(): static
     {
+        $this->stopExecution = false;
         try {
             $html = file_get_contents("https://www.scootsafe.com/");
         } catch (Throwable) {
@@ -30,13 +33,12 @@ class NeuronDataImporter extends DataImporter
             return $this;
         }
 
-        $crawler = new Crawler($html);
-        $this->sections = $crawler->filter("nav.navbar > div > ul > li.nav-city > div > div > ul > li > button");
-        // nav.navbar > div > ul > li.nav-city > div > div > ul > li > button
-        // #search-199-element > li
-        // html body div#app.scootsafe-app div.scootsafe-pagecontent nav.navbar.container div.row ul.navbar-nav li.nav-item.nav-city div.dropdown.dropdown-region div.dropdown-menu.dropdown-menu-end ul#search-199-element.dropdown-result li
+        $pattern = '/{"list":\[.*?};/';
+        preg_match($pattern, $html, $matches);
 
-        if (count($this->sections) === 0) {
+        $this->jsonData = json_decode($matches[0], true);
+
+        if (!$this->jsonData) {
             $this->createImportInfoDetails("204", self::PROVIDER_ID);
 
             $this->stopExecution = true;
@@ -54,42 +56,45 @@ class NeuronDataImporter extends DataImporter
         $mapboxService = new MapboxGeocodingService();
         $existingCityProviders = [];
 
-        foreach ($this->sections as $section) {
-            $cityName = trim($section->nodeValue);
-            $iso = $section->getElementsByTagName('img')->getAttribute('alt');
+        $countries=$this->jsonData["list"];
+        foreach ($countries as $country) {
+            $countryName = $country["name"];
+            $cities = $country["cities"];
+            foreach ($cities as $city){
+                $cityName = $city["name"];
 
-            $city = City::query()->where("name", $cityName)->first();
-            $alternativeCityName = CityAlternativeName::query()->where("name", $cityName)->first();
+                $city = City::query()->where("name", $cityName)->first();
+                $alternativeCityName = CityAlternativeName::query()->where("name", $cityName)->first();
 
-            if ($city || $alternativeCityName) {
-                $cityId = $city ? $city->id : $alternativeCityName->city_id;
+                if ($city || $alternativeCityName) {
+                    $cityId = $city ? $city->id : $alternativeCityName->city_id;
 
-                $this->createProvider($cityId, self::PROVIDER_ID);
-                $existingCityProviders[] = $cityId;
-            } else {
-                $country = Country::query()->where("iso", $iso)->first();
-
-                if ($country) {
-                    $countryName = $country->getAttribute("name");
-                    $coordinates = $mapboxService->getCoordinatesFromApi($cityName, $countryName);
-                    $countCoordinates = count($coordinates);
-
-                    if (!$countCoordinates) {
-                        $this->createImportInfoDetails("419", self::PROVIDER_ID);
-                    }
-
-                    $city = City::query()->create([
-                        "name" => $cityName,
-                        "latitude" => ($countCoordinates > 0) ? $coordinates[0] : null,
-                        "longitude" => ($countCoordinates > 0) ? $coordinates[1] : null,
-                        "country_id" => $country->id,
-                    ]);
-
-                    $this->createProvider($city->id, self::PROVIDER_ID);
-                    $existingCityProviders[] = $city->id;
+                    $this->createProvider($cityId, self::PROVIDER_ID);
+                    $existingCityProviders[] = $cityId;
                 } else {
-                    $this->countryNotFound($cityName, $iso);
-                    $this->createImportInfoDetails("420", self::PROVIDER_ID);
+                    $country = Country::query()->where("name", $countryName)->first();
+
+                    if ($country) {
+                        $coordinates = $mapboxService->getCoordinatesFromApi($cityName, $countryName);
+                        $countCoordinates = count($coordinates);
+
+                        if (!$countCoordinates) {
+                            $this->createImportInfoDetails("419", self::PROVIDER_ID);
+                        }
+
+                        $city = City::query()->create([
+                            "name" => $cityName,
+                            "latitude" => ($countCoordinates > 0) ? $coordinates[0] : null,
+                            "longitude" => ($countCoordinates > 0) ? $coordinates[1] : null,
+                            "country_id" => $country->id,
+                        ]);
+
+                        $this->createProvider($city->id, self::PROVIDER_ID);
+                        $existingCityProviders[] = $city->id;
+                    } else {
+                        $this->countryNotFound($cityName, $countryName);
+                        $this->createImportInfoDetails("420", self::PROVIDER_ID);
+                    }
                 }
             }
         }
