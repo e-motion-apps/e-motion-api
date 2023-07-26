ec6dc5d5596925bb1e1ce952f2ab31c082447eaf
vojcc <kuba.wojcik000@gmail.com>
2023-07-26 11:26:37 +0200

    #69 - redirect back when 405 occurs

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace App\Exceptions;
 
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
+    public function render($request, Exception|\Throwable $e)
+    {
+        if ($e instanceof MethodNotAllowedHttpException) {
+            return back();
+        }
+
+        return parent::render($request, $e);
+    }
 }
