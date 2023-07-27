5c503dcbcb2e8f0dc83415d585b6f1181368e09c
BarTracz <115506125+BarTracz@users.noreply.github.com>
2023-07-27 13:18:58 +0200

    #8 - add city to favorites (#67)

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Favorites;
+use Illuminate\Http\Request;
+use Illuminate\Session\Store as Session;
+
+class FavoritesController extends Controller
+{
+    public function store(Request $request, Session $session): void
+    {
+        $cityId = $request->input("city_id");
+        $userId = $request->user()?->id;
+
+        $favorite = Favorites::firstOrCreate(
+            ["user_id" => $userId, "city_id" => $cityId],
+        );
+
+        if ($favorite->wasRecentlyCreated) {
+            $session->flash("message", "City added to favorites!");
+        } else {
+            $favorite->delete();
+            $session->flash("message", "City removed from favorites!");
+        }
+    }
+
+    public function check(Request $request, int $cityId): bool
+    {
+        $userId = $request->user()?->id;
+
+        return Favorites::where("user_id", $userId)
+            ->where("city_id", $cityId)
+            ->exists();
+    }
+}
