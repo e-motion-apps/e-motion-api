fd85d95edf6db86c53d7b7bd79b24dd07bd8a5a2
BarTracz <bartosz.tracz@studenci.collegiumwitelona.pl>
2023-07-27 14:53:25 +0200

    wip

--- app/Importers/BeamDataImporter.php
+++ app/Importers/BeamDataImporter.php
@@ -31,7 +31,22 @@ class BeamDataImporter extends DataImporter
         }
 
         $crawler = new Crawler($html);
-        $this->sections = $crawler->filter("div .beam-col-main-box");
+
+
+        $this->sections = $crawler->filter("div .find-beam-box");
+
+
+        $crawler->filter('br')->each(function (Crawler $brNode) {
+
+            $brDOMNode = $brNode->getNode(0);
+
+            $spaceTextNode = $brDOMNode->ownerDocument->createTextNode('  ');
+
+            $brDOMNode->parentNode->replaceChild($spaceTextNode, $brDOMNode);
+        });
+
+        $modifiedHtml = $crawler->html();
+
 
         if (count($this->sections) === 0) {
             $this->createImportInfoDetails("204", self::PROVIDER_ID);
@@ -55,12 +70,31 @@ class BeamDataImporter extends DataImporter
 
             foreach ($section->childNodes as $node) {
                 if ($node->nodeName === "div") {
-                    foreach ($node->childNodes as $city) {
-                        if ($city->nodeName === "p") {
-                            $cityName = trim($city->nodeValue);
-                            dump($cityName);
+                    foreach ($node->childNodes as $div) {
+                        if ($div->nodeName === "div") {
+                            foreach ($div->childNodes as $city) {
+                                if ($city->nodeName === "p") {
+                                    $search = ["\u{00A0}", "\u{200D}"];
+                                    $cityName = str_replace($search, '', $city->nodeValue);
+                                    $cityName = preg_replace('/[\p{Hiragana}\p{Katakana}\p{Han}]+/u', '', $cityName);
+                                    $arrayOfCitiesNames = explode("  ", $cityName);
+                                    $arrayOfCitiesNames = array_filter($arrayOfCitiesNames, function ($record) {
+                                        return strlen($record) > 1;
+                                    });
+                                    $arrayOfCitiesNames  = array_filter($arrayOfCitiesNames, function ($record) {
+                                        return strpos($record, "•") === false;
+                                    });
+                                    foreach ($arrayOfCitiesNames as $cityName) {
+                                        $cityName = trim($cityName);
+                                        dump($cityName);
+                                    }
+                                }
+                            }
                         }
                     }
+                }
+                elseif ($node->nodeName === "h4"){
+                    $countryName = $node->nodeValue;
                 }
             }
         }
