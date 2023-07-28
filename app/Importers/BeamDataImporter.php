5ed3fd71dc36fef906362af896f3e9b660347838
BarTracz <bartosz.tracz@studenci.collegiumwitelona.pl>
2023-07-28 08:44:01 +0200

    wip

--- app/Importers/BeamDataImporter.php
+++ app/Importers/BeamDataImporter.php
@@ -18,6 +18,8 @@ class BeamDataImporter extends DataImporter
     protected Crawler $sections;
     private string $countryName;
 
+    private bool $hasEscooters = false;
+
     public function extract(): static
     {
         try {
@@ -67,15 +69,27 @@ class BeamDataImporter extends DataImporter
 
         foreach ($this->sections as $section) {
             $country = null;
-
             foreach ($section->childNodes as $node) {
+                if ($node->nodeName === "h4") {
+                    $countryName = $node->nodeValue;
+                }
                 if ($node->nodeName === "div") {
                     foreach ($node->childNodes as $div) {
                         if ($div->nodeName === "div") {
                             foreach ($div->childNodes as $city) {
-                                if ($city->nodeName === "p") {
+                                if ($city->nodeName === "img" && $city->getAttribute("src") === "https://uploads-ssl.webflow.com/63c4acbedbab5dea8b1b98cd/63d8a5b60da91e7d71298637_map-vehicle-saturn.png") {
+                                    dump("test1");
+                                    dump($city->getAttribute("src"));
+                                    $this->hasEscooters = true;
+                                } else {
+                                    dump("test2");
+                                    dump($city->getAttribute("src"));
+                                    $this->hasEscooters = false;
+                                }
+                                if ($city->nodeName === "p" && $this->hasEscooters == true) {
                                     $search = ["\u{00A0}", "\u{200D}"];
                                     $cityName = str_replace($search, '', $city->nodeValue);
+                                    $cityName = str_replace("Prefecture", '', $city->nodeValue);
                                     $cityName = preg_replace('/[\p{Hiragana}\p{Katakana}\p{Han}]+/u', '', $cityName);
                                     $arrayOfCitiesNames = explode("  ", $cityName);
                                     $arrayOfCitiesNames = array_filter($arrayOfCitiesNames, function ($record) {
@@ -86,15 +100,16 @@ class BeamDataImporter extends DataImporter
                                     });
                                     foreach ($arrayOfCitiesNames as $cityName) {
                                         $cityName = trim($cityName);
-                                        dump($cityName);
+                                        if ($countryName === "Australia") {
+                                            //dump($cityName);
+                                            //dump($this->hasEscooters);
+                                            //dump($countryName);
+                                        }
                                     }
                                 }
                             }
                         }
                     }
-                }
-                elseif ($node->nodeName === "h4"){
-                    $countryName = $node->nodeValue;
                 }
             }
         }
