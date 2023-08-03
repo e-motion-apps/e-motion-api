c91032bf319368912eb13eeb0ba42a8c44b98411
BarTracz <bartosz.tracz@studenci.collegiumwitelona.pl>
2023-08-03 13:51:44 +0200

    fix

--- resources/js/Shared/Components/LanguageSwitch.vue
+++ resources/js/Shared/Components/LanguageSwitch.vue
@@ -6,12 +6,12 @@ const locales = [
   {
     name: 'Polski',
     lang: 'pl',
-    iso: 'pl'
+    iso: 'pl',
   },
   {
     name: 'English',
     lang: 'en',
-    iso: 'gb'
+    iso: 'gb',
   },
 ]
 
@@ -21,7 +21,7 @@ const currentLocale = ref(page.props.locale)
 
 function setLocale(locale) {
   if (currentLocale.value !== locale) {
-    currentLocale.value = locale;
+    currentLocale.value = locale
     router.post(`/language/${locale}`)
   }
 }
@@ -29,12 +29,12 @@ function setLocale(locale) {
 
 <template>
   <div>
-    <div class="flex space-x-2">
-      <button v-for="locale in locales" :key="locale.lang" :class="{
+    <div class="flex pt-1.5 space-x-2">
+      <button class="large flat flags" v-for="locale in locales" :key="locale.lang" :class="{
         'opacity-100': currentLocale === locale.lang,
         'opacity-50': currentLocale !== locale.lang,
       }" :disabled="currentLocale === locale.lang" @click="setLocale(locale.lang)">
-        <i :class="`${locale.iso} flat flag`"></i>
+          <i :class="`${locale.iso} flat flag`" />
       </button>
     </div>
   </div>
