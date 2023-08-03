9012b6e0a0146822709119b1dd081f7eec65960a
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2023-08-03 10:38:21 +0200

    use Mock Handler

--- tests/Unit/BirdDataImporterTest.php
+++ tests/Unit/BirdDataImporterTest.php
@@ -4,16 +4,15 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
-use App\Exceptions\MapboxGeocodingServiceException;
 use App\Importers\BirdDataImporter;
-use App\Models\City;
-use App\Models\CityWithoutAssignedCountry;
-use App\Models\Provider;
 use App\Services\MapboxGeocodingService;
 use Database\Seeders\CitiesAndCountriesSeeder;
 use Database\Seeders\ProviderSeeder;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\MockObject\Exception;
@@ -25,24 +24,19 @@ class BirdDataImporterTest extends TestCase
 
     public function testExtractWithSuccessfulResponse(): void
     {
-        $mockResponse = new Response(200, [], "let features = [
+        $mockResponseBody = "let features = [
 					  {
 			position: new google.maps.LatLng(-31.9523123, 115.861309),
 			type: 'hq'
-		  },
-					  {
-			position: new google.maps.LatLng(45.4215296, -75.6971931),
-			type: 'hq'
-		  },
-					  {
-			position: new google.maps.LatLng(40.813616, -96.7025955),
-			type: 'hq'
-		  },];");
+		  }];";
 
-        $mockHttpClient = $this->createMock(Client::class);
-        $mockHttpClient->method("get")->willReturn($mockResponse);
+        $mockHandler = new MockHandler([
+            new Response(200, [], $mockResponseBody),
+        ]);
+        $handlerStack = HandlerStack::create($mockHandler);
+        $mockHttpClient = new Client(['handler' => $handlerStack]);
+
         $mockMapboxService = $this->createMock(MapboxGeocodingService::class);
-
 
         $dataImporter = new BirdDataImporter($mockHttpClient, $mockMapboxService);
         $result = $dataImporter->extract();
@@ -55,12 +49,14 @@ class BirdDataImporterTest extends TestCase
      */
     public function testExtractWithFailedResponse(): void
     {
-        $mockHttpClient = $this->createMock(Client::class);
+        $mockHandler = new MockHandler([
+            new RequestException("Error communicating with the server", new Request("GET", "https://www.bird.co/map/")),
+        ]);
+
+        $handlerStack = HandlerStack::create($mockHandler);
+        $mockHttpClient = new Client(['handler' => $handlerStack]);
+
         $mockMapboxService = $this->createMock(MapboxGeocodingService::class);
-
-
-        $mockHttpClient->method("get")->willThrowException(new RequestException("Error communicating with the server", new \GuzzleHttp\Psr7\Request("GET", "https://www.bird.co/map/")));
-
 
         $dataImporter = new class($mockHttpClient, $mockMapboxService) extends BirdDataImporter {
             protected function createImportInfoDetails($code, $providerName): void
@@ -73,32 +69,32 @@ class BirdDataImporterTest extends TestCase
         $this->assertTrue($result->hasStoppedExecution());
     }
 
-    /**
-     * @throws MapboxGeocodingServiceException
-     * @throws Exception
-     */
-    public function testCityAddsToCitiesDatabase():void
+    public function testCityAddsToCitiesDatabase(): void
     {
         $this->seed(ProviderSeeder::class);
         $this->seed(CitiesAndCountriesSeeder::class);
 
-        $mockResponse = new Response(200, [], "let features = [
-					  {
-			position: new google.maps.LatLng(-31.9523123, 115.861309),
-			type: 'hq'
-		  },
-					  {
-			position: new google.maps.LatLng(45.4215296, -75.6971931),
-			type: 'hq'
-		  },
-					  {
-			position: new google.maps.LatLng(40.813616, -96.7025955),
-			type: 'hq'
-		  }
-		  ];");
+        $mockResponseBody = "let features = [
+        {
+            position: new google.maps.LatLng(-31.9523123, 115.861309),
+            type: 'hq'
+        },
+        {
+            position: new google.maps.LatLng(45.4215296, -75.6971931),
+            type: 'hq'
+        },
+        {
+            position: new google.maps.LatLng(40.813616, -96.7025955),
+            type: 'hq'
+        }
+    ];";
 
-        $mockHttpClient = $this->createMock(Client::class);
-        $mockHttpClient->method("get")->willReturn($mockResponse);
+        $mockHandler = new MockHandler([
+            new Response(200, [], $mockResponseBody),
+        ]);
+
+        $handlerStack = HandlerStack::create($mockHandler);
+        $mockHttpClient = new Client(['handler' => $handlerStack]);
 
         $mockMapboxService = $this->createMock(MapboxGeocodingService::class);
         $mockMapboxService->method('getPlaceFromApi')->willReturn(["Perth", "Australia"], ["Ottawa", "Canada"], ["Lincoln", "United States"]);
