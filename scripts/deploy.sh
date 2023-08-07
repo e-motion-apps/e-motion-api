26ce312a42eab783aadf17e4e529e7446be0461a
Kamil Piech <kamil.piech@blumilk.pl>
2023-08-07 12:40:16 +0200

    - deploy fixes (#110)
    * - deploy fix
    
    * - fixed app container name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,16 +10,16 @@ echo "Git checkout tag: " $TAG
 git checkout tags/$TAG -f
 
 echo "Execute composer and install packages"
-docker-compose -f $FILE run --rm -u "$(id -u):$(id -g)" php composer install --optimize-autoloader
+docker-compose -f $FILE run --rm -u "$(id -u):$(id -g)" app composer install --optimize-autoloader
 
 echo "Install npm assets"
-docker-compose -f $FILE exec -T node npm install
+docker-compose -f $FILE exec -T app npm install
 
 echo "Build npm assets"
-docker-compose -f $FILE exec -T node npm run build
+docker-compose -f $FILE exec -T app npm run build
 
 echo "Run migrations and create caches"
-docker-compose -f $FILE run --rm -u "$(id -u):$(id -g)" php php artisan migrate --force &&
-    docker-compose -f $FILE run --rm -u "$(id -u):$(id -g)" php php artisan config:cache &&
-    docker-compose -f $FILE run --rm -u "$(id -u):$(id -g)" php php artisan route:cache &&
-    docker-compose -f $FILE run --rm -u "$(id -u):$(id -g)" php php artisan view:cache
+docker-compose -f $FILE run --rm -u "$(id -u):$(id -g)" app php artisan migrate --force &&
+    docker-compose -f $FILE run --rm -u "$(id -u):$(id -g)" app php artisan config:cache &&
+    docker-compose -f $FILE run --rm -u "$(id -u):$(id -g)" app php artisan route:cache &&
+    docker-compose -f $FILE run --rm -u "$(id -u):$(id -g)" app php artisan view:cache
