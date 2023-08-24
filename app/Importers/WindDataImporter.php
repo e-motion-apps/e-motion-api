0edd0d93e518a976f4a674454f07b73f43985b10
BarTracz <115506125+BarTracz@users.noreply.github.com>
2023-08-24 14:24:14 +0200

    #93 - add Wind provider (#127)

---
+++ app/Importers/WindDataImporter.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Importers;
+
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\DomCrawler\Crawler;
+
+class WindDataImporter extends DataImporter
+{
+    protected Crawler $sections;
+
+    public function extract(): static
+    {
+        try {
+            $response = $this->client->get("https://wind.yango.com/official-website/#/rider?lang=gb");
+            $html = $response->getBody()->getContents();
+        } catch (GuzzleException) {
+            $this->createImportInfoDetails("400", self::getProviderName());
+
+            $this->stopExecution = true;
+
+            return $this;
+        }
+
+        $crawler = new Crawler($html);
+        $this->sections = $crawler->filter("body");
+
+        if (count($this->sections) === 0) {
+            $this->createImportInfoDetails("204", self::getProviderName());
+            $this->stopExecution = true;
+        }
+
+        return $this;
+    }
+
+    public function transform(): void
+    {
+        foreach ($this->sections as $section) {
+            foreach ($section->childNodes as $node) {
+                if ($node->nodeName === "script" && $node->hasAttribute("src")) {
+                    $scriptSrc = $node->getAttribute("src");
+                }
+            }
+        }
+        $jsSrc = "https://wind.yango.com/official-website/" . $scriptSrc;
+        $jsContent = file_get_contents($jsSrc);
+
+        $substring_start = strpos($jsContent, "YOUR SECRET SUPERPOWER?");
+        $substring_start += strlen("YOUR SECRET SUPERPOWER?");
+        $size = strpos($jsContent, "Get the app now", $substring_start) - $substring_start;
+        $substring = substr($jsContent, $substring_start, $size);
+        $patternCity = "/\[\"(.*?)\"\]/";
+        $patternCountry = '/:"(.*?)",/';
+        preg_match_all($patternCity, $substring, $matchesCities);
+        $extractedCities = $matchesCities[1];
+        preg_match_all($patternCountry, $substring, $matchesCountries);
+        $extractedCountries = $matchesCountries[1];
+        $extractedCities = str_replace('"', "", $extractedCities);
+
+        $resultArray = array_map(null, $extractedCities, $extractedCountries);
+
+        foreach ($resultArray as $result) {
+            $cityName = explode(",", $result[0]);
+            $countryName = $result[1];
+
+            foreach ($cityName as $city) {
+                if ($countryName === "UK") {
+                    $countryName = "United Kingdom";
+                }
+
+                if ($countryName === "Korea") {
+                    $countryName = "South Korea";
+                }
+                $provider = $this->load($city, ucfirst($countryName));
+
+                if ($provider !== "") {
+                    $existingCityProviders[] = $provider;
+                }
+            }
+        }
+        $this->deleteMissingProviders(self::getProviderName(), $existingCityProviders);
+    }
+}
--- app/Services/DataImporterService.php
+++ app/Services/DataImporterService.php
@@ -22,6 +22,7 @@ use App\Jobs\TierDataImporterJob;
 use App\Jobs\UrentDataImporterJob;
 use App\Jobs\VeoDataImporterJob;
 use App\Jobs\VoiDataImporterJob;
+use App\Jobs\WindDataImporterJob;
 use App\Jobs\ZwingsDataImporterJob;
 use App\Models\ImportInfo;
 use Illuminate\Support\Facades\Bus;
@@ -58,6 +59,7 @@ class DataImporterService
             new UrentDataImporterJob($this->importInfoId),
             new VoiDataImporterJob($this->importInfoId),
             new VeoDataImporterJob($this->importInfoId),
+            new WindDataImporterJob($this->importInfoId),
             new ZwingsDataImporterJob($this->importInfoId),
         ])->finally(function (): void {
             ImportInfo::query()->where("id", $this->importInfoId)->update([
