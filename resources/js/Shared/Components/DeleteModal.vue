9fb655c2f5008fe50f9977e788906cc3c9da5456
BarTracz <bartosz.tracz@studenci.collegiumwitelona.pl>
2023-08-25 13:34:13 +0200

    linter

--- resources/js/Shared/Components/DeleteModal.vue
+++ resources/js/Shared/Components/DeleteModal.vue
@@ -10,16 +10,18 @@ const confirmDelete = () => emits('delete')
   
 <template>
   <div class="fixed inset-0 z-10 flex items-center justify-center bg-black/50">
-    <div class="bg-white rounded-lg p-6 text-center">
-      <h2 class="text-lg font-semibold mb-4">{{ __('Delete record') }}</h2>
+    <div class="rounded-lg bg-white p-6 text-center">
+      <h2 class="mb-4 text-lg font-semibold">
+        {{ __('Delete record') }}
+      </h2>
       <p>{{ __('Are you sure you want to delete this record?') }}</p>
       <div class="mt-4 flex justify-center">
-        <button class="px-3 py-1 mr-2 text-sm text-gray-800 bg-gray-200 hover:bg-gray-300 rounded"
-          @click="closeModal">
+        <button class="mr-2 rounded bg-gray-200 px-3 py-1 text-sm text-gray-800 hover:bg-gray-300" @click="closeModal">
           {{ __('Cancel') }}
         </button>
-        <button class="px-3 py-1 text-sm font-bold text-white bg-rose-500 hover:bg-rose-600 rounded"
-          @click="confirmDelete">
+        <button class="rounded bg-rose-500 px-3 py-1 text-sm font-bold text-white hover:bg-rose-600"
+                @click="confirmDelete"
+        >
           {{ __('Delete') }}
         </button>
       </div>
