1315eea7589ea9fc123a18c816cf8bd02c48324c
BarTracz <bartosz.tracz@studenci.collegiumwitelona.pl>
2023-08-25 13:31:46 +0200

    center text in popup

--- resources/js/Shared/Components/DeleteModal.vue
+++ resources/js/Shared/Components/DeleteModal.vue
@@ -1,27 +1,25 @@
 <script setup>
 import { defineEmits } from 'vue'
 import { __ } from '@/translate'
-  
+
 const emits = defineEmits(['close', 'delete'])
-  
+
 const closeModal = () => emits('close')
 const confirmDelete = () => emits('delete')
 </script>
   
 <template>
   <div class="fixed inset-0 z-10 flex items-center justify-center bg-black/50">
-    <div class="rounded-lg bg-white p-6">
-      <h2 class="mb-4 text-left text-lg font-semibold">
-        {{ __('Delete record') }}
-      </h2>
-      <p class="text-left">
-        {{ __('Are you sure you want to delete this record?') }}
-      </p>
-      <div class="mt-4 flex justify-start">
-        <button class="mr-2 rounded bg-gray-200 px-3 py-1 text-sm font-bold text-gray-800 hover:bg-gray-300" @click="closeModal">
+    <div class="bg-white rounded-lg p-6 text-center">
+      <h2 class="text-lg font-semibold mb-4">{{ __('Delete record') }}</h2>
+      <p>{{ __('Are you sure you want to delete this record?') }}</p>
+      <div class="mt-4 flex justify-center">
+        <button class="px-3 py-1 mr-2 text-sm text-gray-800 bg-gray-200 hover:bg-gray-300 rounded"
+          @click="closeModal">
           {{ __('Cancel') }}
         </button>
-        <button class="rounded bg-rose-500 px-3 py-1 text-sm font-bold text-white hover:bg-rose-600" @click="confirmDelete">
+        <button class="px-3 py-1 text-sm font-bold text-white bg-rose-500 hover:bg-rose-600 rounded"
+          @click="confirmDelete">
           {{ __('Delete') }}
         </button>
       </div>
