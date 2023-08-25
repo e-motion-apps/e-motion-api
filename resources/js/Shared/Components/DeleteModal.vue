95f2dcd9ef18814e8c30cfb6a2043a944335f4eb
BarTracz <bartosz.tracz@studenci.collegiumwitelona.pl>
2023-08-25 14:50:38 +0200

    deletemodal style change

--- resources/js/Shared/Components/DeleteModal.vue
+++ resources/js/Shared/Components/DeleteModal.vue
@@ -1,30 +1,46 @@
+<template>
+  <TransitionRoot as="template" :show="open">
+    <Dialog as="div" class="relative z-10" @close="open = false">
+      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      </TransitionChild>
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                <div class="sm:flex sm:items-start">
+                  <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <ExclamationTriangleIcon class="h-6 w-6 text-red-600" aria-hidden="true" />
+                  </div>
+                  <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">{{ __('Delete record?') }}</DialogTitle>
+                    <div class="mt-2">
+                      <p class="text-sm text-gray-500">{{ __('Record will be permamently deleted.') }}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <button type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto" @click="confirmDelete">{{ __('Delete') }}</button>
+                <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" @click="closeModal" ref="cancelButtonRef">{{ __('Cancel') }}</button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
 <script setup>
-import { defineEmits } from 'vue'
+import { ref, defineEmits } from 'vue'
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { ExclamationTriangleIcon } from '@heroicons/vue/24/outline'
 import { __ } from '@/translate'
 
 const emits = defineEmits(['close', 'delete'])
 
 const closeModal = () => emits('close')
 const confirmDelete = () => emits('delete')
+const open = ref(true)
 </script>
-  
-<template>
-  <div class="fixed inset-0 z-10 flex items-center justify-center bg-black/50">
-    <div class="rounded-lg bg-white p-6 text-center">
-      <h2 class="mb-4 text-lg font-semibold">
-        {{ __('Delete record') }}
-      </h2>
-      <p>{{ __('Are you sure you want to delete this record?') }}</p>
-      <div class="mt-4 flex justify-center">
-        <button class="mr-2 rounded bg-gray-200 px-3 py-1 text-sm text-gray-800 hover:bg-gray-300" @click="closeModal">
-          {{ __('Cancel') }}
-        </button>
-        <button class="rounded bg-rose-500 px-3 py-1 text-sm font-bold text-white hover:bg-rose-600"
-                @click="confirmDelete"
-        >
-          {{ __('Delete') }}
-        </button>
-      </div>
-    </div>
-  </div>
-</template>
