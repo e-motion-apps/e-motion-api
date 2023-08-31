147a850fc6b0597b3fa61fdadb97d0ed3823a896
BarTracz <bartosz.tracz@studenci.collegiumwitelona.pl>
2023-08-31 14:36:47 +0200

    wip

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+use App\Models\City;
+use App\Models\Country;
 
 class DashboardController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        $cityCount = City::count();
+        $countryCount = Country::count();
+        return Inertia::render("Dashboard/Index", [
+            'cityCount' => $cityCount,
+            'countryCount' => $countryCount,
+        ]);
+    }
 }
--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,7 +1,23 @@
 <script setup>
+import AdminNavigation from '@/Shared/Layout/AdminNavigation.vue'
+import { ref } from 'vue'
+import { usePage } from '@inertiajs/inertia-vue3'
 
+const page = usePage();
+
+const cityCount = ref(page.props.cityCount);
+const countryCount = ref(page.props.countryCount);
+
+const props = defineProps({
+  cityCount: Number,
+  countryCount: Number,
+})
 </script>
 <template>
-
-    
+    <div class="flex h-full min-h-screen flex-col md:flex-row">
+        <AdminNavigation :url="page.url" />
+    </div>
+    <div>
+        <p>{{ cityCount }} {{ countryCount }}</p>
+    </div>
 </template>
