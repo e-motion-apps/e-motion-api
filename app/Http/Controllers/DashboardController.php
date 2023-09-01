a88b62fd26fd8fd1f7bb8fa87091f691bed851c1
BarTracz <bartosz.tracz@studenci.collegiumwitelona.pl>
2023-09-01 17:06:49 +0200

    wip

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,21 +1,36 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
+use App\Models\City;
+use App\Models\CityProvider;
+use App\Models\Country;
+use App\Models\User;
 use Inertia\Inertia;
 use Inertia\Response;
-use App\Models\City;
-use App\Models\Country;
 
 class DashboardController extends Controller
 {
     public function index(): Response
     {
-        $cityCount = City::count();
-        $countryCount = Country::count();
+        $userCount = User::count();
+
+        $cityWithProviders = CityProvider::distinct("city_id")->get();
+        $cityId = $cityWithProviders->unique()->pluck("city_id");
+        $cityIdCount = $cityId->count();
+        
+        $countryId = City::whereIn("id", $cityId)->distinct()->pluck("country_id");
+        $countryIdCount = $countryId->count();
+        $countriesWithCitiesWithProviders = Country::whereIn("id", $countryId)->count();
+
         return Inertia::render("Dashboard/Index", [
-            'cityCount' => $cityCount,
-            'countryCount' => $countryCount,
+            "userCount" => $userCount,
+            "cityWithProviders" => $cityWithProviders,
+            "cityCount" => $cityIdCount,
+            "countryCount" => $countryIdCount,
+            "countriesWithCitiesWithProvidersCount" => $countriesWithCitiesWithProviders,
         ]);
     }
 }
--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,23 +1,29 @@
 <script setup>
 import AdminNavigation from '@/Shared/Layout/AdminNavigation.vue'
-import { ref } from 'vue'
 import { usePage } from '@inertiajs/inertia-vue3'
 
 const page = usePage();
 
-const cityCount = ref(page.props.cityCount);
-const countryCount = ref(page.props.countryCount);
-
 const props = defineProps({
-  cityCount: Number,
-  countryCount: Number,
+    userCount: Number,
+    cityWithProviders: Number,
+    cityCount: Number,
+    countryCount: Number,
 })
 </script>
 <template>
     <div class="flex h-full min-h-screen flex-col md:flex-row">
         <AdminNavigation :url="page.url" />
-    </div>
-    <div>
-        <p>{{ cityCount }} {{ countryCount }}</p>
+        <div class="flex w-full md:justify-end">
+            <div class="mt-16 flex h-full w-full flex-col justify-between md:mt-0 md:w-2/3 lg:w-3/4 xl:w-5/6">
+                <div class="m-4 flex flex-col lg:mx-8">
+                    <h1 class="mb-3 text-lg font-bold text-gray-800">
+                        <div>Users count: {{ props.userCount }}</div>
+                        <div>Cities with providers: {{ props.cityCount }}</div>
+                        <div>Countries with providers: {{ props.countryCount }}</div>
+                    </h1>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
