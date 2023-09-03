04e81ea36b822b9f360f72103e46be061cc5579c
BarTracz <bartosz.tracz@studenci.collegiumwitelona.pl>
2023-09-03 15:44:28 +0200

    dashboard finish

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -20,29 +20,31 @@ const props = defineProps({
         <div class="flex w-full md:justify-end">
             <div class="mt-16 flex h-full w-full flex-col justify-between md:mt-0 md:w-2/3 lg:w-3/4 xl:w-5/6">
                 <div class="m-4 flex flex-col lg:mx-8">
-                    <ul class="mt-8 flex w-full flex-col divide-y divide-gray-300" role="list">
-                        <li
-                            class="group flex cursor-pointer flex-col items-start justify-between gap-x-6 pb-1 pt-4 sm:flex-row sm:pb-4">
-                            <span class="text-left">{{ __('Users count:') }}</span>
-                            <span class="text-right">{{ props.userCount }}</span>
-                        </li>
-                        <li
-                            class="group flex cursor-pointer flex-col items-start justify-between gap-x-6 pb-1 pt-4 sm:flex-row sm:pb-4">
-                            <span class="text-left">{{ __('Cities with providers:') }}</span>
-                            <span class="text-right">{{ props.cityCount }}</span>
-                        </li>
-                        <li
-                            class="group flex cursor-pointer flex-col items-start justify-between gap-x-6 pb-1 pt-4 sm:flex-row sm:pb-4">
-                            <span class="text-left">{{ __('Countries with providers:') }}</span>
-                            <span class="text-right">{{ props.countryCount }}</span>
-                        </li>
+                    <ul class="mt-8 flex w-full flex-col" role="list">
+                        
+                        <div class="grid grid-cols-3 gap-4">
+                            <li class="group flex flex-col items-center cursor-pointer pb-4">
+                                <span class="text-xl">{{ __('Users count') }}</span>
+                                <span class="text-4xl font-black mt-2">{{ props.userCount }}</span>
+                            </li>
 
-                        <ul>
-                            <span
-                                class="text-left group flex cursor-pointer flex-col items-start justify-between gap-x-6 pb-1 pt-4 sm:pb-4">{{
-                                    __('Number of cities with individual providers') }}</span>
-                            <li v-for="(count, providerName) in props.providersCount" :key="providerName">
-                                {{ providerName }}: {{ count }}
+                            <li class="group flex flex-col items-center cursor-pointer pb-4">
+                                <span class="text-xl">{{ __('Cities with providers') }}</span>
+                                <span class="text-4xl font-black mt-2">{{ props.cityCount }}</span>
+                            </li>
+                            <li class="group flex flex-col items-center cursor-pointer pb-4">
+                                <span class="text-xl">{{ __('Countries with providers') }}</span>
+                                <span class="text-4xl font-black mt-2">{{ props.countryCount }}</span>
+                            </li>
+                        </div>
+
+                        <hr class="my-4 border-gray-300">
+
+                        <span class="group flex flex-col items-center cursor-pointer pb-4 text-xl">{{ __('Number of cities where the provider is available') }}</span>
+                        <ul class="grid grid-cols-3 gap-4">
+                            <li v-for="(count, providerName) in props.providersCount" :key="providerName" class="group flex flex-col items-center cursor-pointer pb-6">
+                                <span class="text-xl">{{ providerName }}</span>
+                                <span class="text-4xl font-black mt-2">{{ count }}</span>
                             </li>
                         </ul>
                     </ul>
