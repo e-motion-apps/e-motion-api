6cbab361a79473494261f574f5edc18c83947976
Jakub Kermes <114694305+JakubKermes@users.noreply.github.com>
2024-04-04 15:28:09 +0200

    Update config/scramble.php
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>

--- config/scramble.php
+++ config/scramble.php
@@ -6,36 +6,22 @@ use Dedoc\Scramble\Http\Middleware\RestrictedDocsAccess;
 
 return [
     "api_path" => "api",
-
     "api_domain" => null,
-
-    /**
-     * The path where your OpenAPI specification will be exported.
-     */
     "export_path" => "api.json",
-
     "theme" => "light",
-
     "info" => [
         "version" => env("API_VERSION", "0.0.1"),
-
         "description" => "",
     ],
-
     "ui" => [
         "hide_try_it" => false,
-
         "logo" => "",
-
         "try_it_credentials_policy" => "include",
     ],
-
     "servers" => null,
-
     "middleware" => [
         "web",
         RestrictedDocsAccess::class,
     ],
-
     "extensions" => [],
 ];
