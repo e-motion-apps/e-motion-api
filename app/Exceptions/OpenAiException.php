e130971dec692d4955a366481128e66a26b6bd1b
JakubKermes <jakubker22@gmail.com>
2024-04-04 16:08:51 +0200

    changes in exception handling

--- app/Exceptions/OpenAiException.php
+++ app/Exceptions/OpenAiException.php
@@ -8,7 +8,7 @@ use Exception;
 
 class OpenAiException extends Exception
 {
-    public function __construct($message = "OpenAI API connection error", $code = 200)
+    public function __construct($message = "OpenAI API connection error", $code = 500)
     {
         parent::__construct($message, $code);
     }
