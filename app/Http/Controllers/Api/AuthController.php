06c0008b9a2a52bfb4b20a96d3a3bc0bfde904de
Jakub Kermes <114694305+JakubKermes@users.noreply.github.com>
2024-04-05 15:33:37 +0200

    #213 - create api endpoints (#215)
    * Create api endpoint and user validation
    
    needed to fix user roles
    
    * Complete Api endpoints
    
    * fix ProviderController.php
    
    * fix controllers
    
    * cs
    
    * Add responses, small fixes
    
    * Added OpenAPI documentation and responses
    
    * typing
    
    * Update config/scramble.php
    
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>
    
    * typing
    
    * add translation
    
    ---------
    
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,116 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Exception;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Laravel\Socialite\Facades\Socialite;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function store(RegisterRequest $request): JsonResponse
+    {
+        $user = User::create([
+            "name" => $request->input("name"),
+            "email" => $request->input("email"),
+            "password" => Hash::make($request->input("password")),
+        ]);
+
+        return response()->json([
+            "message" => __("User created."),
+        ]);
+    }
+
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $remember = $request->boolean("remember", false);
+
+        if (Auth::attempt([
+            "email" => $request->email,
+            "password" => $request->password,
+        ], $remember)) {
+            $user = Auth::user();
+            $user_id = (string)Auth::id();
+
+            $token_abilities = $this->getUserAbilities($user);
+
+            $token = $user->createToken($user_id, $token_abilities)->plainTextToken;
+
+            return response()->json([
+                $token_abilities,
+                "access_token" => $token,
+            ]);
+        }
+
+        return response()->json([
+            "message" => __("Invalid credentials."),
+        ], Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            "message" => __("Logged out."),
+        ]);
+    }
+
+    public function redirectToProvider(string $provider): JsonResponse
+    {
+        $redirect_url = Socialite::driver($provider)->stateless()->redirect()->getTargetUrl();
+
+        return response()->json([
+            "redirect_url" => $redirect_url,
+        ]);
+    }
+
+    public function handleProviderRedirect(string $provider): JsonResponse
+    {
+        try {
+            $user = Socialite::driver($provider)->user();
+
+            $user = User::firstOrCreate([
+                "email" => $user->getEmail(),
+            ], [
+                "name" => $user->getName(),
+                "password" => Hash::make(Str::password(8)),
+            ]);
+            $token_abilities = $this->getUserAbilities($user);
+
+            $user_id = $user->id->toString();
+            $token = $user->createToken($user_id, $token_abilities)->plainTextToken;
+
+            return response()->json([
+                "access_token" => $token,
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                "message" => __("Login failed."),
+            ]);
+        }
+    }
+
+    private function getUserAbilities(Authenticatable $user): array
+    {
+        $abilities = [];
+
+        if ($user->isAdmin()) {
+            $abilities[] = "HasAdminRole";
+        }
+
+        return $abilities;
+    }
+}
