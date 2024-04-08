634e2c66c977ad5adede8e6c8f9cab4a22290561
Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
2024-04-08 08:17:42 +0200

    lint

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::name('api.')->group(function () {
+Route::name("api.")->group(function (): void {
     Route::middleware("auth:api")->get("/user", fn(Request $request): JsonResponse => new JsonResponse($request->user()));
     Route::get("/providers", [CityProviderController::class, "index"]);
 
